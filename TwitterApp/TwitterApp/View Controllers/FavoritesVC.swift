cf2b0c3f953989c323e64729fc7dced99fd250c2
Jakub Gawecki <jmgawecki@icloud.com>
2021-01-30 22:19:07 +0000

    Revert "deleted"
    This reverts commit cf348471014bf8a01737ae63a748afef39de30ea.

--- TwitterApp/TwitterApp/View Controllers/FavoritesVC.swift
+++ TwitterApp/TwitterApp/View Controllers/FavoritesVC.swift
@@ -32,6 +32,7 @@ final class FavoritesVC: UIViewController {
         configureButtons()
     }
     
+    
     //MARK: - @Objective functions
     
     @objc private func toFavoritesUsersTapped(sender: UIButton) {
@@ -48,6 +49,7 @@ final class FavoritesVC: UIViewController {
 
     
     //MARK: - Private Functions
+    
     private func authoriseAPI() {
         var hasAlreadyLaunched :Bool!
         hasAlreadyLaunched = UserDefaults.standard.bool(forKey: "hasAlreadyLaunched")
